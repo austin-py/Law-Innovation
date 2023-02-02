deaee3e40ce07330a006db441c9068258d9942b4
austin-py <austinporras2023@u.northwestern.edu>
2023-02-02 16:56:48 -0600

    Scrapes 1000+ URLs, doesn't verify the URLs. Next steps written in comments

--- web-scraping/web_scraping.py
+++ web-scraping/web_scraping.py
@@ -1,15 +1,9 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import Select
-from bs4 import BeautifulSoup
 import time 
-
-
 import os 
 
-import requests
-import csv
-
+from selenium import webdriver
+from selenium.webdriver.support.ui import Select
+from bs4 import BeautifulSoup
 
 links = [] 
 path = os.getcwd() + '/utilities/chromedriver'
@@ -18,31 +12,41 @@ op.headless = False
 driver = webdriver.Chrome(path, options= op)
 driver.maximize_window()
 
+#Navigate to webpage 
 driver.get("https://www.fda.gov/inspections-compliance-enforcement-and-criminal-investigations/compliance-actions-and-activities/warning-letters")
 
+#Find, scroll-to, and change the number of letters visible from 10 to 100 
 elems = driver.find_element_by_class_name("form-control.input-sm")
 driver.execute_script("arguments[0].scrollIntoView();", elems)
-print(elems)
-
 elem = Select(driver.find_element_by_class_name("form-control.input-sm"))
 elem.select_by_value('100')
 
 
-time.sleep(2)
-html = driver.page_source
-time.sleep(2)
 
-
-soup = BeautifulSoup(html,'html.parser')
 links = []
-for i in soup.find_all('td'):
-  for j in i.find_all('a', href=True):
-    links.append("https://www.fda.gov/"+j['href'])
-    print("Found the URL:", "https://www.fda.gov/"+j['href'])
+#For 10 iterations (approx 1000 letters) loop through grabbing all the URLS, clicking next, and doing it again. 
+for timer in range(10):
+    time.sleep(2)
+    html = driver.page_source
+    time.sleep(2)
+
+    #Find all links 
+    soup = BeautifulSoup(html,'html.parser')
+    for i in soup.find_all('td'):
+      for j in i.find_all('a', href=True):
+        links.append("https://www.fda.gov/"+j['href'])
+
+    #Click next button 
+    elems = driver.find_element_by_id("datatable_next")
+    driver.execute_script("arguments[0].scrollIntoView();", elems)
+    driver.execute_script("arguments[0].click();", elems)
+    time.sleep(2)
+
+
+for link in links:
+    #Here we want to use the request library to get the text of each link 
+    #Clean that text 
+    #Save it to a file, or into the excel, or somewhere with a name so we can cross reference with excel sheet during later analysis. 
+    pass 
 
 print(len(links))
-
-# for link in links: 
-    # temp = requests.get(link)
-    # soup_temp = BeautifulSoup(temp.text,'html.parser')
-    # print(soup.text)
