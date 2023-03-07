95704cc695af4b31d2b7ccd096b95726f3d1923a
HaritaD <haritajee@gmail.com>
2023-03-07 15:58:11 -0600

    Got back the read more

--- app.py
+++ app.py
@@ -1,35 +1,46 @@
-import os 
+import os
 import time
 
 from flask import Flask, render_template, request, url_for, redirect
-from company_stats import get_cfr_links
+from company_stats import get_cfr_links, get_inspection_info
 import pandas as pd
 
-#from search.search import Search
+# from search.search import Search
 
 app = Flask(__name__)
-print(Flask(__name__))
 inspection_letter_df = pd.read_excel(
-    "/Users/annabeledwards/Law-Innovation-1/web-scraping/data/inspection_letters.xlsx", sheet_name="Sheet1", header=0)
+    "web-scraping/data/inspectionletters.xlsx", sheet_name="Sheet1", header=0)
+
 
 @app.route("/", methods=["POST", "GET"])
 def home():
     if request.method == "POST":
         company_name = request.form["search_term"]
-        return redirect(url_for("cfr",company_name= company_name))
+        return redirect(url_for("cfr", company_name=company_name))
     # try:
     #     os.remove(os.path.expanduser('~/Downloads/search_term.txt'))
     # except:
     #     pass
-    return render_template('index2.html')
+    return render_template('index.html')
 
 
-@app.route("/cfr<company_name>")
+@app.route("/company/<company_name>", methods=["POST", "GET"])
 def cfr(company_name):
-    data = get_cfr_links(company_name, inspection_letter_df)
-    company_name = company_name
-    return render_template('cfr3.html', data = data, company_name = company_name)
-    
+    if request.method == "POST":
+        inspection = request.form["inspection_id"]
+        print(f"INPSECTION ID:{inspection}")
+        return redirect(url_for("more_info", inspection=inspection))
+    elif request.method == "GET":
+        data = get_cfr_links(company_name, inspection_letter_df)
+        company_name = company_name
+        return render_template('timeline.html', data=data, company_name=company_name)
+
+
+@app.route("/info/<inspection>")
+def more_info(inspection):
+    data = get_inspection_info(inspection, inspection_letter_df)
+    return render_template('more_info.html', data=data, inspection_id=inspection)
+
 
 # @app.route('/result', methods = ['POST','GET'])
 # def result():
@@ -46,4 +57,4 @@ if __name__ == "__main__":
     #     os.remove(os.path.expanduser('~/Downloads/search_term.txt'))
     # except:
     #     pass
-    app.run(debug = True)
+    app.run(debug=True)
